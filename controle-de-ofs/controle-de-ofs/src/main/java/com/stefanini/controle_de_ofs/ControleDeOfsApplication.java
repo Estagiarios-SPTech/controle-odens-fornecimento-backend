06a7a74e4fb5c95339a7fd0c82d9ddd1ce92edd4
Shirley Ferreira Lima <sflima2@stefanini.com>
2025-03-26 10:54:01 -0300

    projeto inicialado e configurado

---
+++ controle-de-ofs/controle-de-ofs/src/main/java/com/stefanini/controle_de_ofs/ControleDeOfsApplication.java
@@ -0,0 +1,13 @@
+package com.stefanini.controle_de_ofs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeOfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeOfsApplication.class, args);
+	}
+
+}
---
+++ controle-de-ofs/controle-de-ofs/src/test/java/com/stefanini/controle_de_ofs/ControleDeOfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanini.controle_de_ofs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeOfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
